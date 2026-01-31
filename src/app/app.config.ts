6b816b487cd876bcad2d2f67450d9f0703841c91
Ludvig Qerraxhija <l.qerraxhija@gmail.com>
2026-01-31 20:10:12 +0100

    added angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideAppName, provideAppVersion} from './app-json-data';
 import {version, name} from '../../package.json';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     provideAppName(name),
-    provideAppVersion(version)
+    provideAppVersion(version), provideAnimationsAsync()
   ]
 };
