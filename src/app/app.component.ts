d7ab1bde462de8967977ac736dd1a47f630eda21
Ludvig Qerraxhija <l.qerraxhija@gmail.com>
2026-01-31 22:18:30 +0100

    updated angular to 21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {environment} from '../environments/environment';
 import {ReplaceDashesPipe} from './shared/pipes/replace-dashes.pipe';
-import {CommonModule, TitleCasePipe} from '@angular/common';
+import { CommonModule, TitleCasePipe } from '@angular/common';
 import {APP_NAME, APP_VERSION} from './app-json-data';
 
 @Component({
