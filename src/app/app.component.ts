b04230228431006adb7bf03a99f3609a6962fb3e
Ludvig Qerraxhija <l.qerraxhija@gmail.com>
2026-05-18 18:06:25 +0200

    feat: integrated Tailwind CSS and used Angular Material for enhanced UI
    - Added Tailwind CSS and PostCSS dependencies in package.json.
    - Updated app.component.html to implement a simple sidenav using Angular Material components and utility classes from tailwind.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,33 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {environment} from '../environments/environment';
 import {ReplaceDashesPipe} from './shared/pipes/replace-dashes.pipe';
 import { CommonModule, TitleCasePipe } from '@angular/common';
 import {ANGULAR_CLI_VERSION, APP_NAME, APP_VERSION} from './app-json-data';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   imports: [
     ReplaceDashesPipe,
     TitleCasePipe,
-    CommonModule
+    CommonModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  host: {
+    class: 'flex h-screen'
+  }
 })
 export class AppComponent {
+
+  isSideNavOpen = signal(false);
+  
   projectName = inject(APP_NAME);
   version = inject(APP_VERSION);
   angularCLIVersion = inject(ANGULAR_CLI_VERSION);
