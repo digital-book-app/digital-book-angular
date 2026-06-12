5cb47e82072a7159b642376b6a0e4737effc4cdd
Ludvig Qerraxhija <l.qerraxhija@gmail.com>
2026-06-12 16:59:23 +0200

    feat: add development environment configuration, update index title, and enhance build scripts

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,8 +1,14 @@
-import {environmentCommon} from './environment.common';
-import {Environment} from './environment.model';
+import { environmentCommon } from './environment.common';
+import { Environment } from './environment.model';
 
 export const environment: Environment = {
   ...environmentCommon,
   apiUrl: 'http://my-staging-url',
   env: 'staging',
+  apiKey: 'stagingKey',
+  authDomain: 'stagingDomain',
+  projectId: 'stagingID',
+  storageBucket: 'stagingBucket',
+  messagingSenderId: 'stagingMsgSenderID',
+  appId: 'stagingAppID',
 };
